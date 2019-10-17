1aaa46b9f40a860aa277c49e9015c4627f9a1eac
ahila muthuswamy <azm0180@tigermail.auburn.edu>
2019-10-17 13:12:43 -0500

    Initial commit

---
+++ src/edu/auburn/CachedDataAdapter.java
@@ -0,0 +1,47 @@
+package edu.auburn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CachedDataAdapter {//implements IDataAccess {
+    Map<Integer, ProductModel> cachedProducts = new HashMap<>();
+    IDataAccess adapter;
+
+    public CachedDataAdapter(IDataAccess adapter) {
+        this.adapter = adapter;
+    }
+
+
+ //   @Override
+    public void connect(String path) {
+        this.adapter.connect(path);
+    }
+
+  //  @Override
+    public ProductModel loadProduct(int id) {
+        if (cachedProducts.containsKey(id))
+            return cachedProducts.get(id);
+        else {
+            ProductModel product = adapter.loadProduct(id);
+            cachedProducts.put(id, product);
+            return product;
+        }
+    }
+
+ //   @Override
+    public boolean saveProduct(ProductModel product) {
+        adapter.saveProduct(product);
+        cachedProducts.put(product.mProductID, product);
+        return true;
+    }
+
+  //  @Override
+    public CustomerModel loadCustomer(int d) {
+        return null;
+    }
+
+  //  @Override
+    public boolean saveCustomer(CustomerModel customer) {
+        return false;
+    }
+}
